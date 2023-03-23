e69ad118b42c7943bf6b169e8c34cc3a72c55416
Michael Schwienbacher <schwimmii@hotmail.com>
2023-03-23 17:53:00 +0100

    Cleaning JS

--- js/sign_up.js
+++ js/sign_up.js
@@ -1,32 +1,4 @@
 /**
- * This function is used to SAVE the users into the backend
- * @returns {Promise<void>}
- */
-
-async function saveUsersToBackend() {
-    let allUsersAsString = JSON.stringify(users);
-    await backend.setItem("registeredUsers", allUsersAsString);
-}
-
-/**
- * This function is used to GET all the users from the backend
- * @returns {Promise<void>}
- */
-
-function getSavedUsersFromBackend() {
-    users = JSON.parse(backend.getItem('registeredUsers')) || [];
-}
-
-/**
- *  This function RETURNS all registered users as an ARRAY
- * @returns {array} - All registred users
- */
-
-function getUsersAsArray() {
-    return JSON.parse(backend.getItem("registeredUsers"));
-}
-
-/**
  * This MAIN function is used to verify if the login details are existing/correct
  */
 
@@ -119,17 +91,6 @@ async function pushToUsersArray(users, name, email, password) {
         printSuccessMessage("You have registered successfully");
         setTimeout(function() {window.location.href = "index.html";}, 1000)
     }
-}
-
-/**
- * This function is used to reset the values of the passed input fields
- * @param element01 - field 01
- * @param element02 - field 01
- */
-
-function emptyValues(element01, element02) {
-    element01.value = "";
-    element02.value = "";
 }
 
 /**
@@ -276,15 +237,4 @@ function readOnlyPwInputs() {
     let pwInput02 = document.getElementById("confirm-password");
     pwInput01.readOnly = true;
     pwInput02.readOnly = true;
-}
-
-/**
- * This function is used to get the parameter of the URL
- * @param param - "email" or "success" parameters
- * @returns {string} - returns the value of the URL
- */
-function getUrlParameters(param) {
-    let urlParams = new URLSearchParams(window.location.search);
-    let success = urlParams.get(param);
-    return success;
 }
