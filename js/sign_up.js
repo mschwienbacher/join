4a581fc2fdb251ed6077424f792b6d22746bdcdf
Michael Schwienbacher <schwimmii@hotmail.com>
2023-03-23 13:35:31 +0100

    Preparation inner pages

--- js/sign_up.js
+++ js/sign_up.js
@@ -257,3 +257,37 @@ function readOnlyPwInputs() {
     pwInput01.readOnly = true;
     pwInput02.readOnly = true;
 }
+
+
+/**
+ * This function is used to show the success message after registration and will be hidden after 5 seconds
+ */
+
+function checkIfUserIsRegistered() {
+    let success = getUrlParameters("success");
+    printSuccessMessage(success);
+}
+
+/**
+ * This function is used to get the parameter of the URL
+ * @param param - "email" or "success" parameters
+ * @returns {string} - returns the value of the URL
+ */
+function getUrlParameters(param) {
+    let urlParams = new URLSearchParams(window.location.search);
+    let success = urlParams.get(param);
+    return success;
+}
+
+/**
+ * This function prints the success message if the users register successfully
+ * @param success - URL parameter "success"
+ */
+
+function printSuccessMessage(success) {
+    let successMsg = document.getElementById("success");
+    if(success) {
+        successMsg.innerHTML = "<p>" + success + "</p>";
+        setTimeout(()=>{successMsg.innerHTML = "";},3000);
+    }
+}
