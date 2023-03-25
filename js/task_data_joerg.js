bde008a329f9ba36760f6cdb73b6e446255d6a0b
joergbetz <125571740+joergbetz@users.noreply.github.com>
2023-03-25 17:44:16 +0100

    created tasks data

---
+++ js/task_data_joerg.js
@@ -0,0 +1,83 @@
+let tasksToDo = [
+    {
+        category: "Design",
+        titel: "website redesign",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        inCharge: ["assets/img/dummy.png", "assets/img/dummy.png", "assets/img/dummy.png"],
+        priority: "assets/img/low.svg",
+        subtasks: ["subtask1", "subtask2"],
+        alreadyDone: 1,
+    },
+    {
+        category: "Sales",
+        titel: "analyse sales",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        inCharge: ["assets/img/dummy.png", "assets/img/dummy.png", "assets/img/dummy.png"],
+        priority: "assets/img/low.svg",
+        subtasks: ["subtask1", "subtask2"],
+        alreadyDone: 1,
+    }
+]
+
+let tasksInProgress = [
+    {
+        category: "Backoffice",
+        titel: "check ads",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        inCharge: ["assets/img/dummy.png", "assets/img/dummy.png", "assets/img/dummy.png"],
+        priority: "assets/img/low.svg",
+        subtasks: ["subtask1", "subtask2"],
+        alreadyDone: 1,
+    },
+    {
+        category: "Media",
+        titel: "Video cut",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        inCharge: ["assets/img/dummy.png", "assets/img/dummy.png", "assets/img/dummy.png"],
+        priority: "assets/img/low.svg",
+        subtasks: ["subtask1", "subtask2"],
+        alreadyDone: 1,
+    }
+]
+
+let tasksAwaitFeedback = [
+    {
+        category: "Marketing",
+        titel: "order ads",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        inCharge: ["assets/img/dummy.png", "assets/img/dummy.png", "assets/img/dummy.png"],
+        priority: "assets/img/low.svg",
+        subtasks: ["subtask1", "subtask2"],
+        alreadyDone: 1,
+    },
+    {
+        category: "Design",
+        titel: "Design logo",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        inCharge: ["assets/img/dummy.png", "assets/img/dummy.png", "assets/img/dummy.png"],
+        priority: "assets/img/low.svg",
+        subtasks: ["subtask1", "subtask2"],
+        alreadyDone: 1,
+    }
+]
+
+let tasksDone = [
+    {
+        category: "Marketing",
+        titel: "book ads",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        inCharge: ["assets/img/dummy.png", "assets/img/dummy.png", "assets/img/dummy.png"],
+        priority: "assets/img/low.svg",
+        subtasks: ["subtask1", "subtask2"],
+        alreadyDone: 1,
+    },
+    {
+        category: "Backoffice",
+        titel: "analyse applications",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        inCharge: ["assets/img/dummy.png", "assets/img/dummy.png", "assets/img/dummy.png"],
+        priority: "assets/img/low.svg",
+        subtasks: ["subtask1", "subtask2"],
+        alreadyDone: 1,
+    }
+]
