4c7264d23d45a9351912674938e227cfc3395521
joergbetz <125571740+joergbetz@users.noreply.github.com>
2023-03-28 20:13:25 +0200

    add-task.html form validation and clear function

--- assets/templates/htmlTemplateAdd_Task.js
+++ assets/templates/htmlTemplateAdd_Task.js
@@ -5,3 +5,21 @@ function htmlTemplateSubtasks(newtaskSubtask){
     <span>${newtaskSubtask}</span>
     </div>`;    
 }
+
+function htmlTemplateListAssignedTo(i){
+    return `
+    <li><input type="checkbox" /> ${contacts[i]['name']} ${contacts[i]['second-name']}</li>
+    `;
+}
+
+function htmlTemplateCategory(){
+    return `
+    <ul class="items">
+        <li><input type="checkbox" /> Backoffice</li>
+        <li><input type="checkbox" /> Design</li>
+        <li><input type="checkbox" /> Marketing</li>
+        <li><input type="checkbox" /> Media</li>
+        <li><input type="checkbox" /> Sales</li>
+    </ul>
+    `;
+}
--- js/add-task.js
+++ js/add-task.js
@@ -1,6 +1,11 @@
 
 
 function addTask(){
+    let title = document.getElementById('task-title').value;    
+    let dueDate = document.getElementById('due-date').value;    
+    let taskDescription =document.getElementById('task-description').value;
+    
+    document.getElementById('task-title').value = 'Sag mir warum';
 
 }
 
@@ -14,6 +19,28 @@ function openTaskCategory(){
 }
 
 
+function renderAddTask(){
+    renderListAssignedTo();
+    renderListTaskCategory();
+}
+
+function renderListAssignedTo(){
+    let content = document.getElementById('checkbox-list-assigned-to');
+    content.innerHTML = '';
+    for (let i = 0; i < contacts.length; i++)
+    content.innerHTML += 
+        htmlTemplateListAssignedTo(i);
+}
+
+
+function renderListTaskCategory(){
+    content = document.getElementById('list-task-category');
+    content.innerHTML = '';
+    content.innerHTML =
+        htmlTemplateCategory();
+}
+
+
 function renderSubtasks(){
     let content = document.getElementById('task-subtask');
     newtaskSubtask = content.value;
@@ -23,3 +50,13 @@ function renderSubtasks(){
     }
     content.value = '';
 } 
+
+function clearAddTaskForm(){
+    document.getElementById('task-title').value = '';
+    renderListAssignedTo();
+    document.getElementById('due-date').value = "dd/mm/yyyy";
+    renderListTaskCategory();
+    /* setPriority(); */
+    document.getElementById('task-description').value = '';
+    document.getElementById('ckeckbox-subtasks').innerHTML = '';
+}
