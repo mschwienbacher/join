73ff23712cf59fcded729cc7d22aa991536f0b50
joergbetz <125571740+joergbetz@users.noreply.github.com>
2023-04-09 15:49:47 +0200

    add_task template

--- js/main.js
+++ js/main.js
@@ -122,3 +122,17 @@ function showLogOffBox() {
     let logOffBox = document.getElementById("more-details");
     logOffBox.classList.toggle("showit");
 }
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
